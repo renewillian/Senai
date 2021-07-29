f97d65e3d40a932f0c0b9542a48e169d3119fa41
Renê Nascimento <reneifpr@gmail.com>
2021-07-28 22:48:12 -0300

    segundo commit

--- src/com/company/Duplicidade.java
+++ src/com/company/Duplicidade.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Duplicidade {
 
     public static void main(String[] args) {
+
+        System.out.println("digite o tamanho do seu array :");
+        Scanner scanner = new Scanner(System.in);
+        Integer i = scanner.nextInt();
+        int[] array =  new int[i];
+        for (int j = 0; j < array.length; j++) {
+            System.out.println("valor : " + array[i]);
+        }
 
          }
 }
--- src/com/company/Reversa.java
+++ src/com/company/Reversa.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Reversa {
 
     public static void main(String[] args) {
-        StringBuffer stringReversa = new StringBuffer("1234");
+        System.out.println("digite uma string :");
+        Scanner scanner = new Scanner(System.in);
+        String leitura = scanner.next();
+        StringBuffer stringReversa = new StringBuffer(leitura);
         if (stringReversa.length() % 2 == 0) {
             System.out.println("essa string é par");
         } else {
